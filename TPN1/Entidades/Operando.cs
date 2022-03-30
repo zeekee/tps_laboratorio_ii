5129f44c44040d0b45dfc39eaee59db656ca4b61
zeekee <dev.ezequiel@gmail.com>
2022-03-30 10:32:22 -0300

    first commit

---
+++ TPN1/Entidades/Operando.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Linq;
+
+namespace Entidades
+{
+    public class Operando
+    {
+        private const string ValorInvalido = "Valor inválido";
+        private double numero;
+
+        public string Numero { set { numero = ValidarOperando(value); } }
+
+        public Operando()
+        {
+            numero = 0;
+        }
+
+        public Operando(double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Operando(string strNumero)
+        {
+            Numero = strNumero;
+        }
+
+        private double ValidarOperando(string strNumero)
+        {
+            string fixNumero = strNumero.Replace('.', ',');
+            return double.TryParse(fixNumero, out numero) ? numero : 0;
+        }
+
+        private bool EsBinario(string binario)
+        {
+            return !binario.Any(c => c != '0' && c != '1' && c != '.');
+        }
+
+        private string ObtenerValorEnteroAbsoluto(string numero)
+        {
+            decimal truncateNumber = Math.Truncate(Convert.ToDecimal(numero));
+            return Math.Abs(truncateNumber).ToString();
+        }
+
+        public string BinarioDecimal(string binario)
+        {
+            string resultado;
+
+            if (!string.IsNullOrWhiteSpace(binario) && EsBinario(binario))
+            {
+                resultado = Convert.ToInt32(ObtenerValorEnteroAbsoluto(binario), 2).ToString();
+            }
+            else
+            {
+                resultado = ValorInvalido;
+            }
+
+            return resultado;
+        }
+
+        public string DecimalBinario(string numero)
+        {
+            string resultado;
+
+            if (Int32.TryParse(ObtenerValorEnteroAbsoluto(numero), out int num))
+            {
+                resultado = Convert.ToString(num, 2);
+            }
+            else
+            {
+                resultado = ValorInvalido;
+            }
+
+            return resultado;
+        }
+
+        public string DecimalBinario(double numero)
+        {
+            string resultado;
+
+            try
+            {
+                resultado = Convert.ToString((int)numero, 2);
+            }
+            catch
+            {
+                resultado = ValorInvalido;
+            }
+
+            return resultado;
+        }
+
+        public double OperadorSuma(Operando numeroUno, Operando numeroDos)
+        {
+            return numeroUno.numero + numeroDos.numero;
+        }
+
+        public double OperadorResta(Operando numeroUno, Operando numeroDos)
+        {
+            return numeroUno.numero - numeroDos.numero;
+        }
+
+        public double OperadorMultiplicacion(Operando numeroUno, Operando numeroDos)
+        {
+            return numeroUno.numero * numeroDos.numero;
+        }
+
+        public double OperadorDivision(Operando numeroUno, Operando numeroDos)
+        {
+            return numeroDos.numero != 0 ? numeroUno.numero / numeroDos.numero : double.MinValue;
+        }
+    }
+}
---
+++ TPN1/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,74 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+            cmbOperador.DataSource = new List<string> { string.Empty, "+", "-", "*", "/" };
+        }
+
+        private void FormCalculadora_Load(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private static double Operar(string numero1, string numero2, string operador)
+        {
+            Operando num1 = new(numero1);
+            Operando num2 = new(numero2);
+
+            return Calculadora.Operar(num1, num2, operador[0]);
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = Operar(txtNumero1.Text, txtNumero2.Text, cmbOperador.Text).ToString();
+            lstOperaciones.Items.Add($"{txtNumero1.Text} {cmbOperador.Text} {txtNumero2.Text} = {lblResultado.Text}");
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void Limpiar()
+        {
+            txtNumero1.Clear();
+            txtNumero2.Clear();
+            cmbOperador.Text = string.Empty;
+            lblResultado.Text = "0";
+        }
+
+        private void btnConvertBinario_Click(object sender, EventArgs e)
+        {
+            Operando operando = new();
+            lblResultado.Text = operando.DecimalBinario(lblResultado.Text);
+        }
+
+        private void btnConvertDecimal_Click(object sender, EventArgs e)
+        {
+            Operando operando = new();
+            lblResultado.Text = operando.BinarioDecimal(lblResultado.Text);
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void FormCalculadora_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("¿Está seguro de querer salir?", "Salir",
+                MessageBoxButtons.YesNo) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
