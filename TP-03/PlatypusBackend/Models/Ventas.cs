c4b2876162314c7cf3289f9ad4ab8330ba48682b
zeekee <dev.ezequiel@gmail.com>
2022-05-26 16:40:22 -0300

    fix sql code

---
+++ TP-03/PlatypusBackend/Models/Ventas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BackendPlatypus.Models
+{
+    public class Ventas
+    {
+        public int Id { get; set; }
+
+        public float TotalSale { get; set; }
+
+        public DateTime Created_at { get; set; }
+    }
+}
