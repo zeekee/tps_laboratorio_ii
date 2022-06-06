32ed1ca9f063c8f8c29b1ec117818f3f7f523611
zeekee <dev.ezequiel@gmail.com>
2022-06-06 00:14:44 -0300

    fix

--- TP-03/Platypus/Forms/ProveedoresForm.Designer.cs
+++ TP-03/Platypus/Forms/ProveedoresForm.Designer.cs
@@ -50,6 +50,7 @@ namespace FrontendPlatypus
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -254,11 +255,23 @@ namespace FrontendPlatypus
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(921, 20);
+            this.label2.TabIndex = 73;
+            this.label2.Text = "Si un proveedor esta linkeado a un producto en stock, no se podra eliminar. Prime" +
+    "ro elimine el producto, luego el proveedor.";
+            // 
             // ProveedoresForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1139, 596);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label1);
@@ -311,5 +324,6 @@ namespace FrontendPlatypus
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label2;
     }
 }
